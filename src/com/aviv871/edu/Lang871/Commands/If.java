4ad44d1cd8198a4b49077f4d4c5bd91d2bdbc004
Aviv871 <21aviv12@gmail.com>
2017-01-24 18:42:35 +0200

    Basic if-than statements
    As for now include only single comparisons with numbers:
    ==   !=   <   >  <=    >=
    
    TODO:
    - Add option the compare strings and booleans
    - Support if-elseif-else
    - Support or\and

--- src/com/aviv871/edu/Lang871/Commands/If.java
+++ src/com/aviv871/edu/Lang871/Commands/If.java
@@ -1,5 +1,8 @@
 package com.aviv871.edu.Lang871.Commands;
 
+import com.aviv871.edu.Lang871.Interpreter;
+import com.aviv871.edu.Lang871.Utilities.Math;
+
 public class If implements ICommand
 {
     private static final String code871 = "אם";
@@ -11,8 +14,104 @@ public class If implements ICommand
     }
 
     @Override
-    public void sendParameters(String par, int line)
+    public void sendParameters(String par, int lineNumber)
     {
+        if(!par.contains(" אז ")) throw new RuntimeException("Error with command parameters, missing 'אז', in line: " + lineNumber);
+        String condition = par.substring(0, par.indexOf(" אז "));
+        String command = par.substring(par.indexOf(" אז ") + 4);
 
+        if(checkStringCondition(condition, lineNumber)) Interpreter.initiateLine(command, lineNumber);
+    }
+
+    private boolean checkStringCondition(String con, int lineNumber)
+    {
+        con = con.replaceAll("\\s",""); // Remove all whitespaces
+        String par1, par2, compareType;
+        double value1, value2;
+        char currentChar = con.charAt(0);
+        short currentPosition = 0, startPosition = 0;
+
+        if((currentChar >= 'א' && currentChar <= 'ת') || (currentChar >= '0' && currentChar <= '9')) // Finding The First Variable\Number\Function
+        {
+            while ((currentChar >= 'א' && currentChar <= 'ת') || (currentChar >= '0' && currentChar <= '9'))
+            {
+                if(++currentPosition < con.length())
+                {
+                    currentChar = con.charAt(currentPosition);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            par1 = con.substring(0, currentPosition);
+        }
+        else
+        {
+            throw new RuntimeException("Error with command parameters, non-valid comparison, in line: " + lineNumber);
+        }
+
+        startPosition = currentPosition;
+        if(con.charAt(currentPosition) == '=' || con.charAt(currentPosition) == '!' || con.charAt(currentPosition) == '<' || con.charAt(currentPosition) == '>') // Finding the comparison type
+        {
+            currentPosition++;
+            if(con.charAt(currentPosition) == '=' || con.charAt(currentPosition) == '>' || con.charAt(currentPosition) == '<')
+            {
+                currentPosition++;
+                compareType = con.substring(startPosition, currentPosition);
+            }
+            else
+            {
+                compareType = con.substring(startPosition, currentPosition);
+            }
+        }
+        else
+        {
+            throw new RuntimeException("Error with command parameters, non-valid comparison, in line: " + lineNumber);
+        }
+
+        startPosition = currentPosition;
+        currentChar = con.charAt(currentPosition);
+        if((currentChar >= 'א' && currentChar <= 'ת') || (currentChar >= '0' && currentChar <= '9')) // Finding The Second Variable\Number\Function
+        {
+            while ((currentChar >= 'א' && currentChar <= 'ת') || (currentChar >= '0' && currentChar <= '9'))
+            {
+                if(++currentPosition < con.length())
+                {
+                    currentChar = con.charAt(currentPosition);
+                }
+                else
+                {
+                    break;
+                }
+            }
+            par2 = con.substring(startPosition, currentPosition);
+        }
+        else
+        {
+            throw new RuntimeException("Error with command parameters, non-valid comparison, in line: " + lineNumber);
+        }
+
+        value1 = Math.evaluateArithmeticFromString(par1, lineNumber);
+        value2 = Math.evaluateArithmeticFromString(par2, lineNumber);
+        switch (compareType)
+        {
+            case "==":
+                return value1 == value2;
+            case "!=":
+                return value1 != value2;
+            case "<":
+                return value1 < value2;
+            case ">":
+                return value1 > value2;
+            case "<=":
+                return value1 <= value2;
+            case ">=":
+                return value1 >= value2;
+            default:
+                throw new RuntimeException("Error with command parameters, non-valid comparison, in line: " + lineNumber);
+        }
+
+        // TODO: Add string and boolean comparison or single boolean variable
     }
 }
--- src/com/aviv871/edu/Lang871/Interpreter.java
+++ src/com/aviv871/edu/Lang871/Interpreter.java
@@ -11,32 +11,12 @@ public class Interpreter
     {
         int currentLineNumber = 0;
 
-        newLine:
         for(String line: code)
         {
             currentLineNumber++;
             if(isLineEmpty(line)) continue;
 
-            for(LangKeyWords keyWord: LangKeyWords.values()) // Commands
-            {
-                if (line.startsWith(keyWord.get871Command().get871Code() + " "))
-                {
-                    line = line.substring(keyWord.get871Command().get871Code().length() + 1); // Cutting the command part from the line and leaving only the parameters
-                    keyWord.get871Command().sendParameters(line, currentLineNumber);
-                    continue newLine;
-                }
-            }
-
-            for(String varName: Variable.getVariablesNames()) // Variables
-            {
-                if (line.startsWith(varName))
-                {
-                    LangKeyWords.VARIABLE_POST.get871Command().sendParameters(line, currentLineNumber);
-                    continue newLine;
-                }
-            }
-
-            throw new RuntimeException("Error with command in line: " + currentLineNumber);
+            initiateLine(line, currentLineNumber);
         }
 
         System.out.println(Variable.variables); // For debugging only!
@@ -51,5 +31,29 @@ public class Interpreter
         }
 
         return true;
+    }
+
+    public static void initiateLine(String line, int lineNumber)
+    {
+        for(LangKeyWords keyWord: LangKeyWords.values()) // Commands
+        {
+            if (line.startsWith(keyWord.get871Command().get871Code() + " "))
+            {
+                line = line.substring(keyWord.get871Command().get871Code().length() + 1); // Cutting the command part from the line and leaving only the parameters
+                keyWord.get871Command().sendParameters(line, lineNumber);
+                return;
+            }
+        }
+
+        for(String varName: Variable.getVariablesNames()) // Variables
+        {
+            if (line.startsWith(varName))
+            {
+                LangKeyWords.VARIABLE_POST.get871Command().sendParameters(line, lineNumber);
+                return;
+            }
+        }
+
+        throw new RuntimeException("Error with command in line: " + lineNumber);
     }
 }
